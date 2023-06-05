066ce95a20c4a86e8589a9e7a8d6927f684933c8
fjmiguel <fjmiguel@106-29.info2>
2023-06-05 10:42:07 +0200

    Terminado

--- RepasoEjer-T8/src/ejercicio1/Main.java
+++ RepasoEjer-T8/src/ejercicio1/Main.java
@@ -5,53 +5,89 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
+	/**
+	 * Nombre del producto
+	 */
 	static String nombre;
+	/**
+	 * Precio del producto
+	 */
 	static double precio;
+	/**
+	 * Respuesta del usuario a producto perecedero
+	 */
 	static String respPere;
+	/**
+	 * Dias restantes a caducar de un producto
+	 */
 	static int diasCaducaProducto;
+	/**
+	 * Tipo de producto no perecedero
+	 */
 	static String tipoProductoNoPere;
+	/**
+	 * Coleccion para guardar todos los productos
+	 */
 	static TreeSet<Producto> collection = new TreeSet<Producto>();
+	/**
+	 * Scanner para leer por teclado
+	 */
 	static Scanner read = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		// Opción elegida del menú de opciones
 		int opc;
 
 		do {
+			// Mostramos el menú
 			menu();
+			// Leemos la opción
 			opc = read.nextInt();
 			read.nextLine();
 
 			switch (opc) {
+			// En caso de ser 1, agregaremos un nuevo producto
 			case 1:
+				// Pedimos el nombre y lo leemos
 				System.out.println("Nombre del producto a agregar: ");
 				nombre = read.next();
 				read.nextLine();
+				// Creamos un objeto con el nombre del producto
 				Producto p = new Producto(nombre);
+				// Comprobamos que en la coleccion no se encuentre el producto y pedimos su
+				// precio
 				if (!collection.contains(p)) {
 					System.out.println("Precio del producto: ");
 					precio = read.nextDouble();
 					read.nextLine();
 					do {
+						// Preguntamos si es o no un producto perecedero y leemos la respuesta
 						System.out.println("Es un producto perecedero (Si|No):  ");
 						respPere = read.next();
 					} while (!respPere.equals("Si") && !respPere.equals("No"));
+					// Llamamos al método que solicita datos según si es perecedero o no
 					tipoProducto(nombre, precio, respPere, read);
+					// Si el producto existe, mostramos un mensaje
 				} else {
 					System.out.println("Ya existe");
 				}
 				break;
+			// En el caso 2, mostramos los productos que contiene la coleccion
 			case 2:
 				for (Producto valores : collection) {
 					System.out.println(valores);
 					System.out.println();
 				}
 				break;
+			// En el caso 3, llamammos al método que elimina un producto
 			case 3:
 				eliminarProducto(read);
 				break;
+			// En el caso 4, nos salimos del programa
 			case 4:
 				System.out.println("Saliendo...");
 				break;
+			// Si la opción no es correcta, mostramos un mensaje
 			default:
 				System.out.println("Opción incorrecta");
 			}
@@ -59,6 +95,13 @@ public class Main {
 
 	}
 
+	/**
+	 * Este método se encarga de eliminar un producto, recibiendo su nombre y
+	 * comprobando si existe o no en la coleccion. Si existe lo eliminará y mostrará
+	 * un mensaje, y si no existe, lo mostrará con un mensaje
+	 * 
+	 * @param read
+	 */
 	private static void eliminarProducto(Scanner read) {
 		System.out.println("Nombre del producto a eliminar: ");
 		nombre = read.next();
@@ -71,6 +114,16 @@ public class Main {
 		}
 	}
 
+	/**
+	 * Este métpdo pide al usuario unos datos u otro según si el producto es
+	 * perecedero o no. Después de recibirlos añadirá el nuevo producto resgitrado a
+	 * la coleccion
+	 * 
+	 * @param nombre
+	 * @param precio
+	 * @param respPere
+	 * @param read
+	 */
 	private static void tipoProducto(String nombre, double precio, String respPere, Scanner read) {
 		Producto product = null;
 		switch (respPere) {
@@ -95,6 +148,9 @@ public class Main {
 
 	}
 
+	/**
+	 * Este método muestra el menú de opciones para el usuario
+	 */
 	private static void menu() {
 		System.out.println();
 		System.out.println("MENÚ");
